36c8788c8c571d850a7392b80629e3e5d8a07410
Tang Hoang Griep <hoang@Tangs-Laptop.local>
2025-07-23 14:05:39 +0200

    Add Hints
    Created buttons that show and hide hints

---
+++ src/components/HinweisBox.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="hinweis-box">
+        <button @click="sichtbar = !sichtbar" class="toggle-button">
+            {{ sichtbar ? "▼" : "▶" }} {{ titel }}
+        </button>
+    </div>
+
+    <div v-if="sichtbar" class="hinweis-inhalt">
+        {{ hinweis }}
+    </div>
+</template>
+
+<script setup>
+import {ref} from "vue"
+
+const props = defineProps({
+    titel: String,
+    hinweis: String,
+})
+
+const sichtbar = ref(false)
+</script>
+
+<style scoped>
+    .toggle-button {
+        background: none;
+        border: none;
+        font-weight: bold;
+        font-size: 1rem;
+        color: #33691e;
+        cursor: pointer;
+        padding: 0;
+        margin-bottom: 0.5rem;
+    }
+
+    .hinweis-inhalt {
+        padding-bottom: 8px;
+    }
+
+    li {
+        margin-bottom: 0.3rem;
+    }
+</style>
---
+++ src/components/SideBarRechts.vue
@@ -0,0 +1,37 @@
+<template>
+    <aside class="tipps-box">
+        <div><h3>Hinweise</h3></div>
+        <HinweisBox :titel="'Code starten'" :hinweis="hinweisRaketeStart"/>
+        <HinweisBox :titel="'y-Variable'" :hinweis="hinweisRaketeVariable"/>
+    </aside>        
+        
+</template>
+
+<script setup>
+    import HinweisBox from './HinweisBox.vue';
+
+    const hinweisRaketeStart = "Vergiss nicht, auf die grüne Flagge zu klicken."
+    const hinweisRaketeVariable = "Die y-Variable kennst du bereits aus Koordinatensystemen im Mathematikunterricht"
+
+</script>
+
+<style scoped>
+    .tipps-box {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        width: 15vw;
+        background-color: #fff;
+        box-sizing: border-box;
+        height: 100vh;
+        overflow-y: auto;
+        text-align: left;
+    }
+
+    .tipps-box h3 {
+        margin-top: 0;
+        font-size: 1.2rem;
+        margin-bottom: 1rem;
+    }
+
+</style>
