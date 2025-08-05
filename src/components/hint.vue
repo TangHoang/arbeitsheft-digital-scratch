c441aca246f8eef78f1148fb84f90bc581d79c6f
Tang Hoang Griep <hoang@Tangs-Laptop.local>
2025-08-05 14:10:07 +0200

    add support for hints containing images

--- src/components/hint.vue
+++ src/components/hint.vue
@@ -1,12 +1,15 @@
 <template>
-    <div class="hint-box">
-        <button @click="visible = !visible" class="toggle-button">
-            {{ visible ? "▼" : "▶" }} {{ title }}
-        </button>
-    </div>
+    <div>
+        <div class="hint-box">
+            <button @click="visible = !visible" class="toggle-button">
+                {{ visible ? "▼" : "▶" }} {{ title }}
+            </button>
+        </div>
 
-    <div v-if="visible" class="hint-content">
-        {{ hint }}
+        <div v-if="visible" class="hint-content">
+            <div>{{ hint }}</div>
+            <div class="image-hint" v-if="imageLink"><img :src=imageLink alt=""></div>
+        </div>
     </div>
 </template>
 
@@ -16,6 +19,7 @@ import {ref} from "vue"
 const props = defineProps({
     title: String,
     hint: String,
+    imageLink: String,
 })
 
 const visible = ref(false)
@@ -37,5 +41,16 @@ const visible = ref(false)
         padding-bottom: 8px;
         padding-left: 18px;
         font-size: 1rem;
+    }
+
+    .image-hint {
+        width: auto;
+        height: auto;
+        padding: 5px 5px;
+    }
+    .image-hint img {
+        max-width: 100%;
+        height: auto;
+        display: block;
     }
 </style>
