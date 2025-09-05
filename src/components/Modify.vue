b7e6feb8a9f28a37bfe211ee6779a8cb67b23fc8
Tang Hoang Griep <hoang@Tangs-Laptop.local>
2025-09-05 17:09:04 +0200

    add components for structuring chapters using PRIMM didactic framework

---
+++ src/components/Modify.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="primm-label">Verändern</div>
+    <InfoCardWhite>
+        <div class="exercise-grid">
+            <div class="exercise-content">
+                <slot />
+            </div>
+        </div>
+    </InfoCardWhite>
+
+</template>
+
+<script setup>
+import InfoCardWhite from './InfoCardWhite.vue';
+</script>
+
+
+<style scoped>
+.primm-label {
+    font-weight: 800;
+    padding-left: 1rem;
+    font-style: italic;
+}
+
+.exercise-grid {
+    display: grid;
+    grid-template-columns: 5fr;
+    gap: 1.5rem;
+    width: 100%;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+    padding-bottom: 20px;
+}
+
+.exercise-content {
+    min-width: 0;
+    width: 100%;
+}
+
+.sidebar {
+    min-width: 0;
+    height: 50%;
+    align-self: center;
+}
+</style>
