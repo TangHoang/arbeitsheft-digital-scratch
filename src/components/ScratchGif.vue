016f940bb526f57dd3cc15e1e66c01b59a949635
Tang Hoang Griep <hoang@Mac.fritz.box>
2025-09-06 20:30:47 +0200

    new file

---
+++ src/components/ScratchGif.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="vertical-container">
+        <div class="scratch-image" @mouseenter="hover = true" @mouseleave="hover = false">
+            <img :height="height" :src="hover ? imageUrls[0] : imageUrls[1]" alt="Scratch Bild" class="scratch-img" />
+        </div>
+        <p>Ziehe deinen Mauszeiger auf das Bild, um das GIF zu starten.</p>
+    </div>
+
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+    imageUrls: {
+        type: Array,
+        required: true,
+        validator: (arr) => arr.length === 2
+    },
+    height: String,
+})
+
+const hover = ref(false)
+</script>
+
+<style scoped>
+.scratch-image {
+    width: auto;
+    cursor: pointer;
+}
+
+.vertical-container {
+    display: flex;
+    flex-flow: column;
+
+}
+
+p {
+    font-size: 0.9rem;
+    padding-left: 10px;
+}
+</style>
