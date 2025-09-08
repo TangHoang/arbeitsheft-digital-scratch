9d786fb2f18262131472b557358ec3ed2f27e6aa
Tang Hoang Griep <hoang@Tangs-Laptop.local>
2025-09-08 15:10:38 +0200

    content: update springen chapter

--- src/pages/Springen.vue
+++ src/pages/Springen.vue
@@ -9,7 +9,7 @@
             <template #default>
                 <h3> {{ content.pr.title }}</h3>
                 <SubtaskList :items="content.pr.aufgabe_a" />
-                <ScratchImage :imageUrls="[sprung_bühne, sprung_img]" :height="'300px'" />
+                <ScratchImage :imageUrls="[sprung_bühne, sprung_img]" :height="'200px'" />
                 <StudentAnswer />
                 <SubtaskList :items="content.pr.aufgabe_b" />
                 <ScratchDemo :scratchUrl="'https://scratch.mit.edu/projects/1213169461/embed'" />
@@ -39,22 +39,63 @@
         <Make>
             <template #default>
                 <h3> {{ content.make.title }}</h3>
-                <SubtaskList :items="content.make.aufgabe_a" />
-                <PopUp :projectUrl="SprungProjectUrl" :iframeUrl="'https://scratch.fim.uni-passau.de/scratch/'"
-                    :floating="false" :buttonTitle="'Editor Öffnen'" />
-                <p class="disclaimer"><strong>Wichtig!</strong> Speichere deinen aktuellen Stand auf deinem PC. Im
+                <div class="horizontal-container">
+                    <div class="vertical-container">
+                        <SubtaskList :items="content.make.aufgabe_a" />
+                        <PopUp :projectUrl="SprungProjectUrl" :iframeUrl="'https://scratch.fim.uni-passau.de/scratch/'"
+                            :floating="false" :buttonTitle="'Editor öffnen'" :type="'editor'" />
+                        <PopUp :projectUrl="SprungProjectTestUrl"
+                            :iframeUrl="'https://tanghoang.github.io/whisker-edit/?lng=de'" :buttonTitle="'Testen'"
+                            :type="'test'" />
+                    </div>
+                    <ScratchGif :imageUrls="[sprung_gif, sprung_bühne]" :height="'200px'" />
+                </div>
+
+                <p class="disclaimer">
+                    <strong>Wichtig!</strong> Speichere deinen aktuellen Stand auf deinem PC. Im
                     nächsten Kapitel werden
-                    wir damit weiter arbeiten!</p>
+                    wir damit weiter arbeiten!
+                </p>
             </template>
         </Make>
 
+        <InfoCardPurple :title="'Recap'">
+            <ul class="info-list">
+                <li class="info-row">
+                    <span class="info-text">
+                        <strong>Schleifen: </strong> Du hast Schleifen korrekt im Code identifiziert.
+                    </span>
+                    <img class="info-icon" src="@/assets/green_checkmark.webp" alt="Bild" loading="lazy" />
+                </li>
+
+                <li class="info-row">
+                    <span class="info-text">
+                        <strong>Bedingungen: </strong> Du hast gesehen, dass wir eine Bedingung für den Sprung haben,
+                        und zwar
+                        der Tastendruck der Leertaste.
+                    </span>
+                    <img class="info-icon" src="@/assets/green_checkmark.webp" alt="Bild" loading="lazy" />
+                </li>
+
+                <li class="info-row">
+                    <span class="info-text">
+                        <strong>Kostüme: </strong> Du hast selbst das Ändern des Kostüms beim Drücken der Leertaste
+                        implementiert.
+                    </span>
+                    <img class="info-icon" src="@/assets/green_checkmark.webp" alt="Bild" loading="lazy" />
+                </li>
+            </ul>
+        </InfoCardPurple>
     </div>
 </template>
 
 <script setup>
 import InfoCardOrange from "../components/InfoCardOrange.vue"
+import InfoCardWhite from "../components/InfoCardWhite.vue"
+import InfoCardPurple from "../components/InfoCardPurple.vue"
 import sprung_img from "@/assets/sprung_assets/sprung.png"
 import sprung_bühne from "@/assets/sprung_assets/sprung_bühne.png"
+import sprung_gif from "@/assets/sprung_assets/sprung_costume_change.gif"
 
 import PredictAndRun from "../components/Primm_components/PredictAndRun.vue"
 import Investigate from "../components/Primm_components/Investigate.vue"
@@ -63,11 +104,13 @@ import Make from "../components/Primm_components/Make.vue"
 
 import SubtaskList from "../components/SubtaskList.vue"
 import ScratchImage from "../components/ScratchImage.vue"
+import ScratchGif from "../components/ScratchGif.vue"
 import StudentAnswer from "../components/StudentAnswer.vue"
 import ScratchDemo from "../components/ScratchDemo.vue"
 import PopUp from "../components/PopUp.vue"
 
 const SprungProjectUrl = new URL("@/assets/sprung_assets/Aufgabe_Sprung_PRIMM.sb3", import.meta.url).href;
+const SprungProjectTestUrl = new URL("@/assets/whisker_tests/sprung.js", import.meta.url).href;
 
 
 const content = {
@@ -88,7 +131,7 @@ const content = {
     },
     make: {
         title: "Aufgabe 4",
-        aufgabe_a: ["Immer, wenn der Vogel springt, soll ... ", "a) ein Soundeffekt erklingen", "b) Der Vogel sein Kostüm ändern."]
+        aufgabe_a: ["Immer, wenn der Vogel springt, soll ... ", "a) ... der Vogel sein Kostüm ändern.", "b) ...ein Soundeffekt erklingen "]
     }
 }
 </script>
@@ -103,6 +146,12 @@ const content = {
     flex-flow: row;
     justify-content: space-evenly;
     padding-bottom: 1rem;
+}
+
+.vertical-container {
+    height: 100%;
+    min-width: 60%;
+    max-width: 100%;
 }
 
 .disclaimer {
