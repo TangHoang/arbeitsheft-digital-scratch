8b6b876ba03fb10a54699c74a95e215e55a5f607
Tang Hoang Griep <hoang@Mac.fritz.box>
2025-09-23 17:52:11 +0200

    feature: composable that saves iframe state

---
+++ src/composables/useOverlayIframe.js
@@ -0,0 +1,41 @@
+import { reactive, readonly } from 'vue'
+
+const state = reactive({
+    open: false,
+    showIframe: false,
+    hasOpened: false,
+    type: 'editor',
+    iframeSrc: 'https://scratch.fim.uni-passau.de/scratch/',
+    projectUrl: '',
+    images: null,
+    exercises: null,
+    hints: null,
+})
+
+function openOverlay(opts = {}) {
+    // iframeSrc nur setzen, wenn noch leer, um Reload zu verhindern
+    if (opts.iframeSrc && !state.iframeSrc) state.iframeSrc = opts.iframeSrc
+    if (opts.projectUrl) state.projectUrl = opts.projectUrl
+    if (opts.type) state.type = opts.type
+    state.images = opts.images ?? state.images
+    state.exercises = opts.exercises ?? state.exercises
+    state.hints = opts.hints ?? state.hints
+
+    state.open = true
+    state.showIframe = state.hasOpened
+}
+
+function closeOverlay() { state.open = false }
+function markDownloaded() {
+    state.showIframe = true
+    state.hasOpened = true
+}
+
+export function useOverlayIframe() {
+    return {
+        state: readonly(state),
+        openOverlay,
+        closeOverlay,
+        markDownloaded,
+    }
+}
